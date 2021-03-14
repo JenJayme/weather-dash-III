3717c27a731f6c82ce89966cae65feaef30836fe
Jen Jayme <jeniferjayme@gmail.com>
2021-03-14 03:54:53 -0700

    Added geolocation api query

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ var APIkey = "8b262eaefe86d4d8579b9c93d3ba1dfc";
 var corsProxy = "https://cors-anywhere.herokuapp.com/";
 var today = (moment().format('ddd, MMMM DD, YYYY'));
 console.log("today:", today);
+
 lat = 33.441792;
 lon = -94.037689;
-
 // var queryURL;
 // CURRENT DAY ONLY BY CITY
 // var weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=`;
 // var queryURL = corsProxy + weatherAPI + cityName + "&appid=" + APIkey;
 // var testqueryURL = corsProxy + "http://api.openweathermap.org/data/2.5/weather?q=Novato&units=imperial&appid=8b262eaefe86d4d8579b9c93d3ba1dfc";
-var testqueryURL = corsProxy + `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${APIkey}`;
-
+var testqueryURL = corsProxy + `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly&appid=${APIkey}`;
 
 
 // REDO: 5 DAY FORECAST BY LAT & LON 
-var weatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${APIkey}`;
+var weatherAPI = corsProxy + `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,daily&appid=${APIkey}`;
 var queryURL = weatherAPI + cityName + "&appid=" + APIkey;
 console.log(testqueryURL);
+
+var cityQuery = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIkey}`;
 
 
 eventListeners = () => {
@@ -48,20 +49,17 @@ appendForecast = (array) => {
     console.log("Running appendForecast function on this array:", array);
     for (i=0; i < array.length - 3; i++) {
         let dayWeatherObj = array[i];
-        console.log(dayWeatherObj);
-        // let day = (moment.utc(dayWeatherObj.dt).format('ddd MMMM Do'));
         let UTCday = dayWeatherObj.dt;
         let day = new Date(UTCday*1000);
-        let shortDay = (moment(day).format('ddd MMMM Do'))
-        console.log("day:", day);
+        let shortDay = (moment(day).format('ddd MMMM Do'));
+        let iconImage = `http://openweathermap.org/img/wn/${dayWeatherObj.weather[0].icon}@2x.png`;
         $('#forecast').append(
             `<div class="card" style="width: 20%;">
-                <img src="placeholder.png" class="card-img-top mt-3" alt="weather icon" style="width:80px; margin:auto">
+                <img src="${iconImage}" class="card-img-top mt-3" alt="weather icon" style="width:80px; margin:auto">
                     <div class="card-body">
                         <h5 class="card-title">${shortDay}</h5>
-                        <p class="card-text">High: </p>
-                        <p>Low:</p>
-                        <p>Humidity:</p>
+                        <p class="card-text">Temperature: ${dayWeatherObj.humidity}F<br>
+                        Humidity: ${dayWeatherObj.humidity}%</p>
                     </div>
             </div>`)
 
@@ -85,15 +83,13 @@ getWeather = () => {
         success: function(result) {
             weatherToday = result.current;
             forecastArray = result.daily;
-            console.log("weatherToday:", weatherToday);
-            console.log("forecastArray:", forecastArray);
             $('#description').text(weatherToday.weather[0].description);
-            $('#temp').text(weatherToday.temp);
+            $('#temp').text(weatherToday.temp + "F");
             $('#humidity').text(weatherToday.humidity+"%");
             $('#windSpeed').text(weatherToday.wind_speed + "mph");
             $('#windDirection').text(weatherToday.wind_deg + "deg");
-            // let iconImage = `<img src="http://openweathermap.org/img/wn/${weatherToday.weather[0].icon}@2x.png" />`;
-            // $('#icon').append(iconImage);
+            let iconImage = `<img src="http://openweathermap.org/img/wn/${weatherToday.weather[0].icon}@2x.png" style="width:200px"/>`;
+            $('#icon').append(iconImage);
             $('#uvIndex').text(weatherToday.uvi);
             appendForecast(forecastArray);
             return weatherToday;
